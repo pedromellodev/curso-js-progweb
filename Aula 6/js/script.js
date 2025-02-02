f95f3df4f1493fca78c770b8d496a7822fcf7751
pedromellodev <pedromello19122006@gmail.com>
2025-02-02 00:16:09 -0200

    Aula 6 - Operadores

---
+++ Aula 6/js/script.js
@@ -0,0 +1,17 @@
+/* Operadores
+
+1 - Operadores Aritméticos
+2 - Operadores de Atribuição
+3 - Operadores de Sequencia
+4 - Operadores de Comparação
+5 - Operador Condicional (Ternário)
+6 - Operadores lógicos
+*/
+
+
+var idade, eleitor
+
+idade = 18;
+eleitor = (idade<18) ? "Não eleitor" : "Sim eleitor";
+
+alert("A resposta é: " + eleitor + "A idade dele é: " + idade + "anos.")
